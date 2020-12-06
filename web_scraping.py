ac0ddfe177bb11352385035f8fd668edf2d7201e
Ptotheo <pollestad92@gmail.com>
2020-12-06 14:46:45 +0100

    dokumetasjon

--- web_scraping.py
+++ web_scraping.py
@@ -1,3 +1,9 @@
+#documentasjon https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+#dokumentasjon css https://www.w3schools.com/cssref/css_selectors.asp
+
+
 import requests
 from bs4 import BeautifulSoup as soup
+
+
 
